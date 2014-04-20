69089f89c236b882639e2f5e206c71a02ca093f2
vaishnavh <vaishnavh.nagarajan@gmail.com>
2014-04-20 22:43:20 +0530

    Added output

--- LDA.py
+++ LDA.py
@@ -24,7 +24,7 @@ for line in lines:
     documents[doc_id] = l[1:] # Words in the document
     vocabulary = vocabulary.union(l[1:]) # Update vocabulary
 #We have the documents in memory now!
-topic_per_word = dict()
+topic_per_word = dict() #For each document, you have a list of topics here
 
 #Initializing all counts
 n_d_k = dict() # Per document, how many words a topic: For a key, get a list
@@ -37,7 +37,7 @@ alpha = [2]*k # ALPHA value
 
 for word in vocabulary:
     beta[word] = 2
-    beta_sum += 0
+    beta_sum += 2
 
 for topic in xrange(k):
     dummy = dict()
@@ -57,7 +57,6 @@ for doc_id in documents.keys():
         n_k[topic] += 1
         n_k_w[topic][word] += 1
         words_per_topic[topic] += 1
-        topic_for_doc += [topic]
     n_d_k[doc_id] = words_per_topic
     topic_per_word[doc_id] = topic_for_doc
 
@@ -77,11 +76,14 @@ for x in xrange(50):
                 q = n_k_w[t][word] + beta[word]
                 r = n_k[t] + beta_sum
                 probab += [float(p*q)/r]
-            probab = probab/sum(probab)
-            topic = list(numpy.random.multinomial(1, probab)).index(1)
+            pr = [p/sum(probab) for p in probab]
+            topic = list(numpy.random.multinomial(1, pr)).index(1)
+            topic_per_word[doc_id][i] = topic #Reassign new topic
+            # Update counts accordingly
             n_d_k[doc_id][topic] += 1
             n_k_w[topic][word] += 1
             n_k[topic] += 1
+    print x
 
 
 
---
+++ save.py
@@ -0,0 +1,14 @@
+
+
+f = open("n_d_k.py","w")
+f.write("n_d_k = " + repr(n_d_k) + "\n")
+f = open("n_k_w.py","w")
+f.write("n_k_w = " + repr(n_k_w) + "\n")
+f = open("n_k.py","w")
+f.write("n_k = " + repr(n_k) + "\n")
+f = open("topic_per_word.py","w")
+f.write("topic_per_word = " + repr(topic_per_word) + "\n")
+f = open("vocabulary.py","w")
+f.write("vocabulary = " + repr(vocabulary) + "\n")
+f = open("documents.py","w")
+f.write("documents = " + repr(documents) + "\n")
